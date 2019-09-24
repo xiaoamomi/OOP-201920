31ee9c4622483314238818341b26d100932c9b61
xiaoamomi <55754343+xiaoamomi@users.noreply.github.com>
2019-09-24 17:35:03 +0100

    bug is fixed

--- lab1_dynamic_typing.py
+++ lab1_dynamic_typing.py
@@ -11,7 +11,7 @@ while my_variable < 20:
                 my_variable +=1
                 print(my_variable)
         else:
-                my_vairable = (my_variable + 20)
+                my_variable = (my_variable + 20)
                 print('new')
 
 
